e8ba3de09b2457ea6af4b94bd884f4b9e15760ba
Sudhanshu Gupta <sudhanshu11gupta@gmail.com>
2021-03-23 21:39:52 +0530

    new start

---
+++ add_constants.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+# building a computational graph
+# creating constant nodes
+
+node1 = tf.constant(3.0, tf.float32)
+node2 = tf.constant(4.0)
+# print(node1,node2)
+
+# create a session of tensorflow object
+sess = tf.Session()
